4ee60bcd1d2171ffade89c40767dd72f063d323d
David Felipe <felipe30.10@outlook.com>
2021-10-08 18:38:08 -0300

    background gradient

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -2,23 +2,17 @@ import React from 'react';
 import { 
     View,
     Text, 
-    Image,
-    StatusBar
+    Image
 } from  'react-native';
 
-import { ButtonIcon } from '../../components/Buttonicon'
+import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles'
 
 export function SignIn() {
     return(
+        
         <View style={ styles.container }>
-            <StatusBar 
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
-
             <Image 
                 source={IllustrationImg}
                 style={styles.image}
@@ -27,9 +21,9 @@ export function SignIn() {
 
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {`\n`}
-                    suas jogatinas {`\n`}
-                    facilmente
+                    Conect-se {'\n'}
+                    e Organize suas {'\n'}
+                    jogatinas
                 </Text>
 
                 <Text style={styles.subtitle}>
@@ -43,5 +37,6 @@ export function SignIn() {
                 />
             </View>
         </View>
+        
     )
 }
